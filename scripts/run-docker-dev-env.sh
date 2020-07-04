1d3991e9b1ccc8f827b5be585983bcd876f0fef0
Mark-Anthony Hutton <mark-anthony.hutton@opsysinc.com>
2020-07-04 16:53:14 -0600

    Added docker-compose.yml and run-docker-dev-env.sh for simple docker development and testing.

---
+++ scripts/run-docker-dev-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script is for running a development environment of the rails application
+
+# Get script's directory.
+# "It will work as long as the last component of the path used to find the script is not a
+# symlink (directory links are OK)"
+# Ref: https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Navigate to development docker environment directory
+cd "${DIR}/../docker/development" || exit
+docker-compose up -d
+docker-compose logs -f
